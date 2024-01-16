c78f3298f37cfcc85ce4a117b27835dcd5b80e6a
jterry94 <terryj@iit.edu>
2024-01-15 22:08:38 -0600

    Initial Commit

---
+++ Multidimensional Integration/Multidimensional_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multidimensional_IntegrationApp.swift
+//  Multidimensional Integration
+//
+//  Created by Jeff_Terry on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Multidimensional_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
