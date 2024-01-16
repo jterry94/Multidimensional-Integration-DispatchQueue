d3ddfb73f6b6d343fbba5a53facd31a5cd03d738
jterry94 <terryj@iit.edu>
2024-01-16 14:32:29 -0600

    Working DispatchQueue example

---
+++ Multidimensional Integration/ArrayStdDev.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayStdDev.swift
+//  Multidimensional Integration
+//
+//  Created by Jeff_Terry on 1/15/24.
+//
+
+import Foundation
---
+++ Multidimensional Integration/MonteCarloIntegration.swift
@@ -0,0 +1,8 @@
+//
+//  MonteCarloIntegration.swift
+//  Multidimensional Integration
+//
+//  Created by Jeff_Terry on 1/15/24.
+//
+
+import Foundation
